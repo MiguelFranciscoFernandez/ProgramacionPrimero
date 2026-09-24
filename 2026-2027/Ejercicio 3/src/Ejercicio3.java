8d6f88c14a796ea2a27f6ae427a7646edf1d9465
Miguel_Francisco_Fernandez <mfranciscof01@iesalbarregas.es>
2026-09-24 18:05:40 +0200

    Tareas 3

---
+++ 2026-2027/Ejercicio 3/src/Ejercicio3.java
@@ -0,0 +1,121 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
+    /*
+     * Ejercicio 1: Construye un programa que pida dos números al usuario e indique
+     * cuál es mayor o si son
+     * iguales.
+     */
+    public static void ejercicio1() {
+        System.out.println("=== Ejercicio 1 ===");
+        System.out.print("Introduce el primer número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Introduce el segundo número: ");
+        int num2 = scanner.nextInt();
+        if (num1 > num2) {
+            System.out.println("El primer número es mayor");
+        } else if (num1 < num2) {
+            System.out.println("El segundo número es mayor");
+        } else {
+            System.out.println("Los números son iguales");
+        }
+        System.out.println();
+    }
+
+    /*
+     * Ejercicio 2: Construye un programa que pida un número al usuario
+     * y nos indique si es par o impar.
+     */
+    public static void ejercicio2() {
+        System.out.println("=== Ejercicio 2 ===");
+        System.out.print("Introduce un número: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("El número es par");
+        } else {
+            System.out.println("El número es impar");
+        }
+        
+        System.out.println();
+    }
+
+    /*
+     * Ejercicio 3: Construye un programa que pida dos números al usuario
+     * y muestre el resultado de su división.
+     * Si el segundo número es 0, debe mostrar un mensaje de error.
+     */
+    public static void ejercicio3() {
+        System.out.println("=== Ejercicio 3 ===");
+        System.out.print("Introduce el primer número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Introduce el segundo número: ");
+        int num2 = scanner.nextInt();
+        if (num2 == 0) {
+            System.out.println("Error: No se puede dividir entre 0");
+        } else {
+            System.out.println("El resultado de la división es: " + (num1 / num2));
+        }
+
+        System.out.println();
+    }
+
+    /*
+     * Ejercicio 4: Construye un programa que pida dos números al usuario. Debe
+     * calcular el resultado de
+     * elevar el primero (base) al segundo (exponente) teniendo en cuenta lo
+     * siguiente:
+     * o Si el exponente es cero el resultado es 1
+     * o En cualquier otro caso calcula la potencia (Math.pow(base,exponente) es la
+     * función que
+     * calcula la potencia)
+     */
+    public static void ejercicio4() {
+        System.out.println("=== Ejercicio 4 ===");
+
+        System.out.println();
+    }
+
+    /*
+     * Ejercicio 5: Construye un programa que exprese si un triángulo es isósceles,
+     * equilátero o escaleno y
+     * si es rectángulo. Para ello debe recibir por teclado 3 valores
+     * correspondiente a la medida de sus
+     * lados.
+     */
+    public static void ejercicio5() {
+        System.out.println("=== Ejercicio 5 ===");
+
+        System.out.println();
+    }
+
+    /*
+     * Ejercicio 6: Un instituto está organizando un viaje y te piden que construyas
+     * un programa que
+     * calcule el coste total del viaje y lo que le supone a cada alumno en función
+     * de lo siguiente:
+     * o Si llegan a 50 alumnos o más, deberán pagar 40€ por alumno.
+     * o De 30 a 49 alumnos, deberán pagar 48€ por alumno.
+     * o De 20 a 29 alumnos, deberán pagar 56€ por alumno.
+     * o Menos de 20 alumnos, deberán abonar un total de 2000 euros en total
+     */
+    public static void ejercicio6() {
+        System.out.println("=== Ejercicio 6 ===");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // ejercicio1();
+        // ejercicio2();
+        // ejercicio3();
+        // ejercicio4();
+        // ejercicio5();
+        // ejercicio6();
+
+        scanner.close();
+    }
+}
